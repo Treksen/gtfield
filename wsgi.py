ec8fa0c13b701b41229219816c05de8772a019cd
Treksen <namsaoffice01@gmail.com>
2026-04-03 23:09:38 +0300

    fix port binding

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,9 @@
-"""
-Production entry point for Gunicorn.
-Run with: gunicorn wsgi:application
-"""
 from dotenv import load_dotenv
 load_dotenv()
 
 from app import app as application
 
 if __name__ == '__main__':
-    application.run()
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    application.run(host='0.0.0.0', port=port)
